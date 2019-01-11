784a71e45f77e19891b8cbd29b4b8bb13d4fc15e
Alex Len <alexlen01@users.noreply.github.com>
2019-01-10 22:21:38 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/SpringbootPpmtoolApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPpmtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPpmtoolApplication.class, args);
+	}
+
+}
+
