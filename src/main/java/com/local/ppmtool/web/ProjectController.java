aac5f9cf8e64e6e08ffcffcb010a3596fb602887
Alex Len <alexlen01@users.noreply.github.com>
2019-01-20 15:05:10 -0500

    Updated controller

--- src/main/java/com/local/ppmtool/web/ProjectController.java
+++ src/main/java/com/local/ppmtool/web/ProjectController.java
@@ -20,7 +20,7 @@ public class ProjectController {
 	
 	@PostMapping("")
 	public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
-		Project project1 = projectService.saveOrUpdateProject(project);
-		return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
+		projectService.saveOrUpdateProject(project);
+		return new ResponseEntity<Project>(project, HttpStatus.CREATED);
 	}
 }
