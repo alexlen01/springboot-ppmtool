2a1e52b197f10c770e39f706eac48d2b3c2c3c68
Alex Len <alexlen01@users.noreply.github.com>
2019-01-20 15:06:29 -0500

    Renamed application

--- src/main/java/com/local/ppmtool/PPMToolApplication.java
+++ src/main/java/com/local/ppmtool/PPMToolApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootPPMToolApplication {
+public class PPMToolApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootPPMToolApplication.class, args);
+		SpringApplication.run(PPMToolApplication.class, args);
 	}
 
 }
